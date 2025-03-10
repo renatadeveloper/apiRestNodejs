10f803920aef93e318904b2d79a87c10df3ab0f1
Renata Careno <renata_dev@hotmail.com>
2025-03-10 18:07:03 -0300

    initialize project with basic server setup and dependencies

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+// function calculaIdadeUsuario() {
+
+//     const prompt = require('prompt-sync')();
+//     let ano = new Number(prompt("Digite seu ano de nascimento: "))
+
+//     const currentYear  = new Date().getFullYear()
+
+//     let resu = currentYear - ano
+
+//     console.log(`${resu}`)
+
+// }
+
+// calculaIdadeUsuario()
